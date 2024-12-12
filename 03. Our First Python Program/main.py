2848122d8cc3fc5cbb28ab52a23469f4e36b7a2e
RISHII7 <rishikesh.palande07@gmail.com>
2024-12-12 19:18:07 +0530

    03. Our First Python Program

---
+++ 03. Our First Python Program/main.py
@@ -0,0 +1,8 @@
+# This is a single-line comment. Comments are ignored by the Python interpreter.
+# They are used to explain the code for better understanding.
+
+# The print() function displays text or output on the screen.
+print("Hello, World!")  # This will print the text "Hello, World!" to the console.
+
+# You can add another print statement to display more messages.
+print("Welcome to Python programming!")  # Prints a welcome message.
